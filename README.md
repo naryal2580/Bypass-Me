91ccdf419a4fe8209815586263521dc793be4f6c
Nick <nick@nick.nick>
2021-04-28 08:10:06 +0545

    Initial commit, without any fix.

---
+++ README.md
@@ -0,0 +1,9 @@
+# Bypass Me
+
+This is a simple CTF challenge to byapass path traversal. Flag is just there as a placeholder and, to provide a dopamine kick to the person who bypassed it. Payload is what you're supposed to submit.
+
+If you can bypass it, do raise an Issue.
+
+And, if you have a fix. Send a PR :)
+
+RED vs BLUE
---
+++ main.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import RedirectResponse, FileResponse
+from os.path import isdir, isfile
+
+
+app = FastAPI()
+
+
+@app.get('/')
+def return_file(filename: str):
+    filepath = f'./root/{filename}'
+
+    if filepath.split('.')[-1] not in ('html', 'txt'):
+        raise HTTPException(
+                status_code=406,
+                detail='I will NOT let you open it.'
+            )
+
+    if not isdir(filepath) and isfile(filepath):
+        return FileResponse(filepath)
+
+    raise HTTPException(
+            status_code=404,
+            detail=f"Requested file('/{filename}') does not exist."
+        )
+
+
+@app.get('/')
+def return_index():
+    return RedirectResponse(url='/?filename=index.html', status_code=301)
+
+
+@app.get('/favicon.ico')
+def fake_favicon():
+    return ''
+
